8617c2cb469acdb2357fef5d8e6b38a6c53e1ea8
laferiaines <inesdebrito_@hotmail.com>
2020-11-04 21:33:40 +0000

    Adding express and port

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const Telegraf = require("telegraf");
 const fs = require("fs");
 
+
+
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
 const PSWD = "ADMIN"
 let alreadyBought = false;
 
@@ -142,6 +158,5 @@ bot.command("text", (ctx) => {
         }
     }
 })
-
 
 bot.launch();
